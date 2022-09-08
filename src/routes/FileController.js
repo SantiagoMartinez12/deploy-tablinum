f8cadf08ddff4d1bd8de0607876da17fe95a580d
SantiagoMartinez12 <santiagovalentino3@gmail.com>
2022-09-08 10:26:09 -0300

    asdas

--- src/routes/FileController.js
+++ src/routes/FileController.js
@@ -17,29 +17,20 @@ class FileController
     res.send('File uploaded!');
   }); */
     const archivo = req.files
-    console.log(req.files)
-    
     const fileName = archivo.name;
     const path = '/var/www/pdf/' + fileName;
     console.log(path)
-    try {
-      archivo.mv(path, (error) => {
-        if (error) {
-          console.error(error);
-          res.writeHead(500, {
-            'Content-Type': 'application/json'
-          });
-          res.end(JSON.stringify({ status: 'error', message: error }));
-            return;
-          }
-          return res.status(200).send({ status: 'success', path: './' + fileName , filename: fileName});
-       });
-     } catch (e) {
-       res.status(500).json({
-         error: true,
-         message: e.toString()
-       });
-     } 
+    if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+    
+    archivo.mv(path, function(err) {
+      if (err)
+        return res.status(500).send(err);
+  
+      res.send('File uploaded!');
+    })
+     
+     
   }
 }
 
