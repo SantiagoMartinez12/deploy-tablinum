93d94355d620a9e628ecdbf05382588e7aa51783
SantiagoMartinez12 <santiagovalentino3@gmail.com>
2022-09-08 10:05:22 -0300

    prueba pdf

--- src/routes/FileController.js
+++ src/routes/FileController.js
@@ -3,10 +3,24 @@ class FileController
 {
   subirArchivo = async (req, res, next) =>
   {
-   
-    const archivo = req.files.image
+    if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+    const path = '../pdf/' + sampleFile.name;
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(path, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+   /*  const archivo = req.files
+    console.log(req.files)
+    
     const fileName = archivo.name;
-    const path = '/var/www/pdf/' + fileName;
+    const path = '../pdf/' + fileName;
     console.log(path)
     try {
       archivo.mv(path, (error) => {
@@ -25,7 +39,7 @@ class FileController
          error: true,
          message: e.toString()
        });
-     }
+     }  */
   }
 }
 
