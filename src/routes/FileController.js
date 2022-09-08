eaf8458e03587d3915feda9e52630d9b055dd019
SantiagoMartinez12 <santiagovalentino3@gmail.com>
2022-09-08 10:19:08 -0300

    asdas

--- src/routes/FileController.js
+++ src/routes/FileController.js
@@ -3,7 +3,7 @@ class FileController
 {
   subirArchivo = async (req, res, next) =>
   {
-    if (!req.files)
+  /*   if (!req.files)
     return res.status(400).send('No files were uploaded.');
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
@@ -15,12 +15,12 @@ class FileController
       return res.status(500).send(err);
 
     res.send('File uploaded!');
-  });
-   /*  const archivo = req.files
+  }); */
+    const archivo = req.files
     console.log(req.files)
     
     const fileName = archivo.name;
-    const path = '../pdf/' + fileName;
+    const path = '/var/www/pdf/' + fileName;
     console.log(path)
     try {
       archivo.mv(path, (error) => {
@@ -39,7 +39,7 @@ class FileController
          error: true,
          message: e.toString()
        });
-     }  */
+     } 
   }
 }
 
